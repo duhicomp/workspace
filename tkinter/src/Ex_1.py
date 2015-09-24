905d5caa43357ec614013793a5e81ef910ffaeb4
duhicomp <m.makhzomi@gmail.com>
2015-09-24 10:56:24 -0400

    Colors.py: File added, demonstrate tkinte inter color, and interesting grid packing
    Blue Relay: POC files, might need some update

--- tkinter/src/Ex_1.py
+++ tkinter/src/Ex_1.py
@@ -1,20 +1,21 @@
-'''
+"""
 Created on Dec 20, 2014
 
 @author: Duhi
-'''
+"""
+import tkinter
 #Testing 
 def test_func():
     return
 
-from tkinter import *
 
-class Application(Frame):
+
+class Application(tkinter.Frame):
     def say_hi(self):
         print("hi there, everyone!")
     
     def createWidgets(self):
-        self.QUIT = Button(self)
+        self.QUIT = tkinter.Button(self)
         self.QUIT["text"] = "QUIT"
         self.QUIT["fg"] = "green"
         self.QUIT["command"] = self.quit
@@ -22,11 +23,11 @@ class Application(Frame):
         self.QUIT.pack({"side":"left"})
         
     def __init__ (self,master=None):
-        Frame.__init__(self,master)
+        tkinter.Frame.__init__(self,master)
         self.pack()
         self.createWidgets()
         
-root = Tk()
+root = tkinter.Tk()
 app = Application(master=root)
 app.mainloop()
 root.destroy()
