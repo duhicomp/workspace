e2d1b1fc8ddb0f58817219eef0da09685fbb3093
duhicomp <m.makhzomi@gmail.com>
2015-09-24 10:45:35 -0400

    Update MegaMillion_GUI.py

--- Mega_Million/src/MegaMillion_GUI.py
+++ Mega_Million/src/MegaMillion_GUI.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 '''
 Created on Dec 10, 2014
-
 @author: Duhi
 '''
 
 # from Tkinter import Tk, Frame, BOTH
  
 from tkinter import Frame, BOTH, Tk
-from tkinter.ttk import Button, Style
+from tkinter.ttk import Button, Style, Label
+
 
 class MM_Window(Frame):
   
@@ -26,12 +26,22 @@ class MM_Window(Frame):
         self.style = Style()
         self.style.theme_use("default")
 
-        self.pack(fill=BOTH, expand=1)
+        # self.pack(fill=BOTH, expand=1)
 
         quitButton = Button(self, text="Quit",
             command=self.quit)
         quitButton.place(x=200, y=50)
-            
+        buttons_list = []
+        g=0
+        c=0
+        for i in range(1,52):
+            crnt_button = Label(self, text=str(i),background=FF0000)
+            crnt_button.grid(row=g, column=c)
+            buttons_list.append(crnt_button)
+            c += 1
+            if i%10 ==0:
+                g +=1
+                c=0
     def centerWindow(self):
       
         w = 290
