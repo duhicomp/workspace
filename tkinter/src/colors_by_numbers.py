8ab972e06f514760128110be2edd86f8e17c4262
duhicomp <m.makhzomi@gmail.com>
2015-09-25 15:54:23 -0400

    Another Tkinter color example. button colors varies from green to read (255 levels)

---
+++ tkinter/src/colors_by_numbers.py
@@ -0,0 +1,70 @@
+__author__ = 'mabdul-aziz'
+#"#%02x%02x%02x" % (128, 192, 200)
+
+import Tkinter as tk
+
+
+
+class VerticalScrolledFrame(tk.Frame):
+    """A pure Tkinter scrollable frame that actually works!
+
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+    """
+    def __init__(self, parent, *args, **kw):
+        tk.Frame.__init__(self, parent, *args, **kw)
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+        vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = tk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=tk.NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
+
+
+root = tk.Tk()
+root.title("Scrollable Frame Demo")
+root.configure(background="gray99")
+
+scframe = VerticalScrolledFrame(root)
+scframe.pack()
+
+for i in range(0,255):
+    btn = tk.Button(scframe.interior, height=1, width=20, relief=tk.FLAT,
+        bg="#%02x%02x%02x" %(i, (254-i), 0), fg='ghost white',
+        font="Dosis", text=str(i))
+    btn.pack(padx=10, pady=0, side=tk.TOP)
+
+#def openlink(i):
+#    print lis[i]
+
+root.mainloop()
